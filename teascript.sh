be946bf7bbe26b4897fe0318695bab4eebc41661
Vihan <blue1086@gmail.com>
2015-12-24 23:05:04 -0800

    Update teascript.sh

--- teascript.sh
+++ teascript.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-install_teascript () {
+if [ ! -d "TeaScript" ]; then
+  echo "TeaScript not installed. Installing TeaScript..."
   mkdir TeaScript
   wget -O TeaScript/macros.js https://raw.githubusercontent.com/vihanb/TeaScript/master/macros.js
   wget -O TeaScript/props.json https://github.com/vihanb/TeaScript/blob/master/props.json
   wget -O TeaScript/teascript.js https://github.com/vihanb/TeaScript/blob/master/v/2/teascript.js
   sed -i -e 's/window/this/g' -e 's/\/\*props\.json\*\//JSON.parse(read("props.json"))/g' TeaScript/teascript.js
   printf "var shoco={};var console={warn:print,log:print,error:print}" > TeaScript/sh.js
-}
-if [ ! -d "TeaScript" ]; then; install_teascript; fi
+fi
 cd TeaScript;
 read -p "Code: " Code
 read -p "Input: " Input
